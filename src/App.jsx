f38da5e5ca40d9216289211c1aa36a555a5e0984
Álvaro Márquez <117214600+alvaromd99@users.noreply.github.com>
2023-05-31 13:11:45 +0200

    First routes and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import { Route } from 'react-router-dom'
 import './App.css'
+import { Routes } from 'react-router-dom'
+
+const Home = () => <h1>Home</h1>
+
+const SearchPage = () => <h1>Search Page</h1>
 
 function App() {
 	return (
-		<>
-			<h1>Chollos</h1>
-		</>
+		<div className='App'>
+			<header>
+				<h1>Bargains 🤑</h1>
+				<nav>
+					<ul>
+						<li>
+							<a href='/search-page'>Search Page</a>
+						</li>
+						<li>
+							<a href='/home'>Home</a>
+						</li>
+					</ul>
+				</nav>
+			</header>
+			<Routes>
+				<Route path='/home' element={<Home />} />
+				<Route path='/search-page' element={<SearchPage />} />
+			</Routes>
+		</div>
 	)
 }
 
